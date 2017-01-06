4a5c0afe5fd3c3e63bb6d2078a00f5d80baecf3b
C.S. Jiang <christine.s.jiang@gmail.com>
2017-01-05 19:43:53 -0500

    updating for heroku

--- server/db/config.js
+++ server/db/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	DATABASE_URI: 'postgres://localhost:5432/mofa',
+	DATABASE_URI: 'postgres://yqvoafxzafijee:ac95883032ce366328416545231b65d118aebdf5fcd13a0ef35678b562299305@ec2-54-235-240-92.compute-1.amazonaws.com:5432/d7qit6mvu8cauk',
 }
